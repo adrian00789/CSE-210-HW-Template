8d9db34deac0bd21af11891a8c0ef157417b9db5
Adrian00789 <adrian00789@gmail.com>
2023-04-18 22:45:01 -0500

    Assigments with stretch challenges

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Core assigments
         Console.Write("What is your grade? ");
         string grade = Console.ReadLine();
         int gradeNumber = int.Parse(grade);
@@ -29,7 +30,34 @@ class Program
         {
             letter = "F";
         }
-        Console.WriteLine($"Your grade is: {letter}");
+
+        // This is to know if we use the + or - symbol depending of the grade
+        string sign = "";
+        int lastDigit = gradeNumber % 10;
+        
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        // This is to ensure there is not A+ or F+/-
+        if (gradeNumber >= 93 || gradeNumber <= 60)
+        {
+            sign = "";
+        }
+    
+
+
+        // Display the message depending of the grade
+        Console.WriteLine($"Your grade is: {letter}{sign}");
 
 
         if (gradeNumber >= 70)
