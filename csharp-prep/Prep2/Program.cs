96defd55055c2e9145f8d526849226bf6ab31638
Adrian00789 <adrian00789@gmail.com>
2023-04-18 22:16:06 -0500

    Core requirement assigment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+        string letter = "";
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
