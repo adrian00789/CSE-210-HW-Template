28c86ce840c437c356853b4b73565510e6d313e0
Adrian00789 <adrian00789@gmail.com>
2023-04-25 22:07:27 -0500

    Core Assigments

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+
+        // Core Assigments
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed it! ");
+            }
+
+
+        }
     }
 }
