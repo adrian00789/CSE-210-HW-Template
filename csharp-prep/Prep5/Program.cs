1719eac1184a670cbc35614de0d71b9bd1bf5ebe
Adrian00789 <adrian00789@gmail.com>
2023-04-26 00:20:06 -0500

    Core Assigments

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,54 @@ using System;
 
 class Program
 {
+
+    //Call all functions
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
     }
+
+
+
+
+    //Functions
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program! ");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        return favoriteNumber;
+    }
+
+    static int SquareNumber(int favoriteNumber)
+    {
+        int squareOperation = favoriteNumber * favoriteNumber;
+        return squareOperation;
+    }
+
+    static void DisplayResult(string userName, int squareOperation)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squareOperation} ");
+    }
+
+
+
 }
